f5dc04da4d58326d31ae229bda66c71ffe4fd4e8
Sc0rpT41l <bram.geens2007@telenet.be>
2025-04-11 20:31:45 -0100

    Directory checker summary

---
+++ dir_checker.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+while true; do
+	1) checksum of every file in specified dir
+	2) put checksum with corresponding filename in temp file
+	3) compare last checksum of files with newer checksum of file 
+	4) make backup of this new version of file and log this in log file with timestamp
+
+
+done
