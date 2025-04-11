1c8f51877cb0609c6205cec3ddd33205f0d77d1f
Sc0rpT41l <bram.geens2007@telenet.be>
2025-04-11 19:42:41 -0100

    Finalised some things

--- backup.sh
+++ backup.sh
@@ -29,6 +29,22 @@ backup_db () {
         echo "Backup successful!"
 }
 
+# Check if file is run with sudo permissions
+if [[ $(id -u) -eq 0 ]]; then
+	:
+elif [[ $(id -u) -ne 0 ]]; then
+	echo "Root privileges are necessary, please login as root."
+	exit 0
+else
+	echo "Something went wrong while checking privileges."
+	exit 0
+fi
+
+# Start the mysql service
+echo "Mysql service starting..."
+sudo service mysql start
+echo "Mysql service started successfully"
+
 while true; do
 
 	read -p "Do you want to save a file/directory or a database?" kind_to_backup
@@ -37,10 +53,7 @@ while true; do
 
 			read -p "What file/directory do you want to backup?: " f_d_to_backup
 			read -p "Where do you want to back it up?: " f_d_where_to_backup
-	##################### MAKE SURE USER HAS TO CONFIRM THIS IS WHAT HE WANTS TO BACKUP #####################
-			echo "${f_d_to_backup} and ${f_d_where_to_backup}"
-	##################### MAKE SURE USER HAS TO CONFIRM THIS IS WHAT HE WANTS TO BACKUP #####################
-
+			# echo "${f_d_to_backup} and ${f_d_where_to_backup}" this line was just to check
 			while true; do
 				# Check if the file to backup exists
 				if [ -e "$f_d_to_backup" ]; then
@@ -76,7 +89,7 @@ while true; do
 					fi
 				else
 					echo "${f_d_to_backup} doesn't exist"
-					read -p "Give a valid thing to backup: " f_d_to_backup
+					read -p "Give a valid file or directory to backup: " f_d_to_backup
 				fi
 			done
 		exit 0
@@ -89,9 +102,7 @@ while true; do
 			sudo mysql -u root -p"$db_password" -e "SHOW DATABASES;"
 			read -p "What database do you want to backup?: " db_to_backup
 	        	read -p "Where do you want to back it up?: " db_where_to_backup
-		##################### MAKE SURE USER HAS TO CONFIRM THIS IS WHAT HE WANTS TO BACKUP #####################
-			echo "${db_to_backup} and ${db_where_to_backup}"
-		##################### MAKE SURE USER HAS TO CONFIRM THIS IS WHAT HE WANTS TO BACKUP #####################
+			# echo "${db_to_backup} and ${db_where_to_backup}" This line was just to check
 
 			while true; do
 		                if sudo mysql -u root -p"$db_password" -e "SHOW DATABASES;" | tail -n +2 | grep -Fxq "$db_to_backup"; then
@@ -136,9 +147,8 @@ while true; do
 		;;
 		#########################################################################################################
 		#########################################################################################################
-			*)
-
+		*)
 			read -p "Please give a valid answer. (y/n) " kind_to_backup
-			;;
-		esac
-	done
+		;;
+	esac
+done
