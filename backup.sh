96f19405e746ab1ec8ea5c37e2a1eda451a47500
Sc0rpT41l <bram.geens2007@telenet.be>
2025-04-11 14:42:35 -0100

    Preparing for database backup option part 2

--- backup.sh
+++ backup.sh
@@ -1,5 +1,24 @@
 #! /bin/bash
 
+# Making the backup function for files/directories with namegiving process
+backup_file_dir () {
+        # Add timestamp to backup filename
+        gen_date=$(date '+%d-%m-%Y')
+        spec_date=$(date -d "+3 hours" +"%H-%M") # To account for the 3 hours delay on the clock
+        backup_filename="${what_to_backup}_${gen_date}_${spec_date}"
+        path_to_backup="${where_to_backup}/${backup_filename}"
+        # Real copying part
+        cp "$what_to_backup" "$path_to_backup"
+        echo "Backup succesful!"
+}
+
+# Making the backup function for databases with namegiving process
+backup_db () {
+
+
+}
+
+
 read -p "Do you want to save a file/directory or a database?" kind_to_backup
 
 case "$kind_to_backup" in
@@ -15,14 +34,7 @@ case "$kind_to_backup" in
 			if [ -e "$what_to_backup" ]; then
 				# Check if the backup location exists
 				if [ -e "$where_to_backup" ]; then
-					# Add timestamp to backup filename
-					gen_date=$(date '+%d-%m-%Y')
-					spec_date=$(date -d "+3 hours" +"%H-%M") # To account for the 3 hours delay on the clock
-					backup_filename="${what_to_backup}_${gen_date}_${spec_date}"
-					path_to_backup="${where_to_backup}/${backup_filename}"
-					# Real copying part
-					cp "$what_to_backup" "$path_to_backup"
-					echo "Backup succesful!"
+					backup_file_dir
 					ls -all "$where_to_backup"
 					break
 				else
@@ -34,24 +46,20 @@ case "$kind_to_backup" in
 								mkdir "$where_to_backup"
 								echo "Directory succesfully created!"
 								echo "Attempting to backup to this location..."
-								# Add timestamp to backup filename
-								gen_date=$(date '+%d-%m-%Y')
-								spec_date=$(date -d "+3 hours" +"%H-%M") # To account for the 3 hours delay>
-								backup_filename="${what_to_backup}_${gen_date}_${spec_date}"
-								path_to_backup="${where_to_backup}/${backup_filename}"
-								#Real copying part
-								cp "$what_to_backup" "$path_to_backup"
-								echo "Backup succesful!"
+								backup_file_dir
 								ls -all "$where_to_backup"
 								exit 0
 								;;
+
 							n|N|no|No|NO) 
 								read -p "Please give a valid place to backup to: " where_to_backup
 								break
 								;;
+
 							*)
 								read -p "Please give a valid answer. (y/n)"
 								;;
+
 						esac
 					done
 				fi
@@ -60,10 +68,11 @@ case "$kind_to_backup" in
 				read -p "Give a valid thing to backup: " what_to_backup
 			fi
 		done
+	;;
 
 	database|Database|DATABASE|db|DB|Db|dB)
 
-	jfkljdsklfjdqs
+	
 	fjksdlfjdq
 	dfjkqlsmdfjk
 	sjfdklqdf
