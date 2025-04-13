d2b19cc2340e83a251531ab0f2a29c417faa9ad4
Sc0rpT41l <bram.geens2007@telenet.be>
2025-04-13 19:15:27 -0100

    Directory checker coding part 5

--- dir_checker.sh
+++ dir_checker.sh
@@ -1,25 +1,40 @@
 #! /bin/bash
 
+###########################TO DO###########################
+# 1) Check if last line of dir_checker.log is the same as now to be able to backup the new files too
+#    Checksum doesn't take new files into account just checks for changes
+# 2) Copy changed files to this directory
+###########################################################
+
 # Date to add to log filename
 gen_date=$(date +%d-%m-%y)
 
 # Remove FAILED file to avoid confusion
 rm $HOME/log/FAILED_${gen_date}.log 2>/dev/null
 
-# Check if last line of dir_checker.log is the same as now to be able to backup the new files too
-# Checksum doesn't take new files into account just checks for changes
+# 1) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 
 sha256sum -c $HOME/log/dir_checker_${gen_date}.log 2>/dev/null | grep "FAILED" > $HOME/log/FAILED_${gen_date}.log
 find $1 -type f -print0 | xargs -0 -n1 'sha256sum' > /home/kali/log/dir_checker_${gen_date}.log
 
 if [[ -s /home/kali/log/FAILED_${gen_date}.log ]]; then
-	echo "Changes were made, file is not empty."
+	echo "Changes were made, FAILED is not empty."
 	cat /home/kali/log/FAILED_${gen_date}.log
-	if  [[ -d Backup ]]; then
-		# Copy changed files to this directory
+	if [[ -d Backup && -w Backup ]]; then
+		# 2) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+	elif [[ ! -w Backup ]]; then
+		mkdir BACKUP
+		# 2) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+	else
+		mkdir Backup
+		# 2) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+	fi
 else
-	echo "No changes were made, file is empty."
+	echo "No changes were made, FAILED is empty."
 fi
 
 
