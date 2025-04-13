3e6cfc5d72c9ce28e785e1ecce58b8e78334bec9
Sc0rpT41l <bram.geens2007@telenet.be>
2025-04-13 16:17:06 -0100

    Directory checker summary part 3

--- dir_checker.sh
+++ dir_checker.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+Run this every minute or so
+
 for every file in spec dir true; do
 	1) checksum of every file in specified dir
 	2) put checksum with corresponding filename in temp file
-	3) compare last checksum of files with newer checksum of file 
+	3) compare last checksum of files with newer checksum of file
 	4) make backup of this new version of file and log this in log file with timestamp
 	5) if file doesn't already have a checksum in temp file make a new one and backup this file
 
