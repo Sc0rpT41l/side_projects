a414dc2b6bdad66612602650345050c2eba5f1e4
Sc0rpT41l <bram.geens2007@telenet.be>
2025-04-13 18:08:06 -0100

    Directory checker coding part 1

--- dir_checker.sh
+++ dir_checker.sh
@@ -1,13 +1,30 @@
 #! /bin/bash
 
-Run this every minute or so
+# Run this every minute or so
 
-for every file in spec dir true; do
-	1) checksum of every file in specified dir
-	2) put checksum with corresponding filename in temp file
-	3) compare last checksum of files with newer checksum of file
-	4) make backup of this new version of file and log this in log file with timestamp
-	5) if file doesn't already have a checksum in temp file make a new one and backup this file
+# Date to add to log filename
+gen_date=$(date +%d-%m-%y)
+
+find $1 -type f -print0 | xargs -0 -n1 'sha256sum' > /var/log/dir_checker_${gen_date}.log
+sha256sum -c /var/log/dir_checker_${gen_date}.log | grep "FAILED" > /var/log/FAILED_${gen_date}.log
+
+if [ -s /var/log/FAILED_${gen_date}.log ]; 
+	echo "No changes made"
+	exit 0
+else
 
 
-done
+fi
+
+
+
+
+
+
+
+
+#v	1) checksum of every file in specified dir
+#v	2) put checksum with corresponding filename in temp file
+#	3) compare last checksum of files with newer checksum of file
+#	4) make backup of this new version of file and log this in log file with timestamp
+#	5) if file doesn't already have a checksum in temp file make a new one and backup this file
