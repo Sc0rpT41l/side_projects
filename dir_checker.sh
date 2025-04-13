f57c526e7a0ed53e005c55098f7173a0c33b45eb
Sc0rpT41l <bram.geens2007@telenet.be>
2025-04-13 18:40:02 -0100

    Directory checker coding part 3, put TO DO list in file

--- dir_checker.sh
+++ dir_checker.sh
@@ -6,13 +6,19 @@ gen_date=$(date +%d-%m-%y)
 # Remove FAILED file to avoid confusion
 rm /home/kali/log/FAILED_${gen_date}.log
 
+# Check if last line of dir_checker.log is the same as now to be able to backup the new files too
+# Checksum doesn't take new files into account just checks for changes
+
 
 sha256sum -c /home/kali/log/dir_checker_${gen_date}.log | grep "FAILED" > /home/kali/log/FAILED_${gen_date}.log
 find $1 -type f -print0 | xargs -0 -n1 'sha256sum' > /home/kali/log/dir_checker_${gen_date}.log
 
 
+
+
 if [[ -s /home/kali/log/FAILED_${gen_date}.log ]]; then
 	echo "Changes were made, file is not empty."
+	cat /home/kali/log/FAILED_${gen_date}.log
 else
 	echo "No changes were made, file is empty."
 fi
@@ -21,11 +27,8 @@ fi
 
 
 
-
-
-
 #v	1) checksum of every file in specified dir
 #v	2) put checksum with corresponding filename in temp file
-#	3) compare last checksum of files with newer checksum of file
+#v	3) compare last checksum of files with newer checksum of file
 #	4) make backup of this new version of file and log this in log file with timestamp
 #	5) if file doesn't already have a checksum in temp file make a new one and backup this file
