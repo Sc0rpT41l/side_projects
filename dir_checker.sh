7a1973ff0dad3dd995212f8703ae2fefca154187
Sc0rpT41l <bram.geens2007@telenet.be>
2025-04-13 21:04:03 -0100

    Directory checker coding part 6

--- dir_checker.sh
+++ dir_checker.sh
@@ -1,31 +1,38 @@
 #! /bin/bash
 
+#########################ARGUMENTS#########################
+# $1 => directory to check
+# $2 => name for new backup folder, please give full path
+##########################################################
+
 ###########################TO DO###########################
-# 1) Check if last line of dir_checker.log is the same as now to be able to backup the new files too
-#    Checksum doesn't take new files into account just checks for changes
+# ////1) Checksum doesn't take new files into account just checks for changes
+# 2) Make sure that the dir structure is kept!!!
 # 2) Copy changed files to this directory
 # 3) Show the changes made in changed.log (like git does)
 # 4) Put this script inside cronjobs for every 5 minutes or something like that
 # 5) Make it so that there will always be a backup dir to backup to by putting numbers behind backup dir name
 # 6) Finalise by zipping the whole backup
+# 7) Make second given argument name for backup dir
 ###########################################################
 
-# Date to add to log filename
-gen_date=$(date +%d-%m-%y)
+# Timestamp for log with changes made and to add to log filename
+gen_date=$(date '+%d-%m-%Y')
+spec_date=$(date -d "+3 hours" +"%H-%M") # To account for the 3 hours delay on the clock
 
-# Timestamp for log with changes made
-get_date () {
-        gen_date=$(date '+%d-%m-%Y')
-        spec_date=$(date -d "+3 hours" +"%H-%M") # To account for the 3 hours delay on the clock
-}
+
+# Copy all files to the backup directory if it´s empty wich means it´s the first time
+if [[ -d $2 && -z "$(ls -A $2)" ]]; then # Dir exists and empty
+	cp -r $1 $2
+elif [[ ! -d $2 ]]; then # Dir not exist and empty
+	mkdir $2
+	cp -r $1 $2
+else # Dir exists and NOT empty => NOT first time
+	:
+fi
 
 # Remove FAILED file to avoid confusion
 rm $HOME/log/FAILED_${gen_date}.log 2>/dev/null
-
-# Check for new files by comparing last line containing last file
-
-# 1) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
 
 sha256sum -c $HOME/log/dir_checker_${gen_date}.log 2>/dev/null | grep "FAILED" > $HOME/log/FAILED_${gen_date}.log
 find $1 -type f -print0 | xargs -0 -n1 'sha256sum' > /home/kali/log/dir_checker_${gen_date}.log
@@ -33,18 +40,7 @@ find $1 -type f -print0 | xargs -0 -n1 'sha256sum' > /home/kali/log/dir_checker_
 if [[ -s /home/kali/log/FAILED_${gen_date}.log ]]; then
 	echo "Changes were made, FAILED is not empty."
 	cat /home/kali/log/FAILED_${gen_date}.log
-	if [[ -d Backup && -w Backup ]]; then
-		# 2) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-	elif [[ ! -w Backup ]]; then
-		mkdir BACKUP
-		# 2) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-	else
-		mkdir Backup
-		# 2) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-	fi
+	# 2) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 else
 	echo "No changes were made, FAILED is empty."
 fi
