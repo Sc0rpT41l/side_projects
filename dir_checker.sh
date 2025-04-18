9cd4d3eef1a62a158c6ea025b605cb0b86c59787
Sc0rpT41l <bram.geens2007@telenet.be>
2025-04-18 18:02:47 -0100

    Directory checker coding part 26, cleaned some variables up

--- dir_checker.sh
+++ dir_checker.sh
@@ -15,9 +15,9 @@
 # ////6) Finalise by zipping the whole backup, less often than running this script (every 12.00h and 00.00h => cronjob should be run at this instant
 # ////7) Make second given argument name for backup dir
 # ////8) Make it so that FAILED log is cleared after old files have been renewed
-# 9) Make all /home/kali/ ... user input dependable!!!!
-# 10) Make often-used paths into variables
-# 11) Make sure script doesn't flip when deleted files can´t be backupped anymore
+# ////9) Make all /home/kali/ ... user input dependable!!!!
+# ////10) Make often-used paths into variables
+# ////11) Make sure script doesn't flip when deleted files can´t be backupped anymore
 # 12) Make the script like a watcher or something like that
 # 13) Show the deleted files in the changes log
 ###########################################################
@@ -25,6 +25,7 @@
 #--------------------------
 # Paths put into variables
 #--------------------------
+
 # Snapshot folder names
 SNAPSHOT="${HOME}/log/.snapshot"
 current=$(mktemp)
@@ -38,6 +39,12 @@ last_part_dir_check2=$(echo ${2} | rev | cut -d "/" -f 1 | rev)
 # Timestamp for log with changes made and to add to log filename
 gen_date=$(TZ=Europe/Paris date '+%d-%m-%Y') # Day, month and year
 spec_date=$(TZ=Europe/Paris date '+%H:%M') # Hours and minutes
+
+# Some more random paths
+h_new_files_p="$HOME/log/.new_files.txt"
+changes_log_p="$HOME/log/changes.log"
+FAILED_file_p="$HOME/log/FAILED_${gen_date}.log"
+
 
 ###########################################################
 
@@ -54,6 +61,7 @@ elif [[ ! -d $2 ]]; then # Dir not exist and empty
 else # Dir exists and NOT empty => NOT first time
 	:
 fi
+
 
 ###########################################################
 
@@ -72,22 +80,22 @@ fi
 find $1 -type f | sort > "$current"
 
 # Compare and show newly added files
-comm -13 "$SNAPSHOT" "$current" > $HOME/log/.new_files.txt
-if [[ -s $HOME/log/.new_files.txt ]]; then
+comm -13 "$SNAPSHOT" "$current" > $h_new_files_p
+if [[ -s $h_new_files_p ]]; then
 	echo "New files detected"
-	echo "===== [ Timestamp: $(TZ=Europe/Paris date '+%Y-%m-%d %H:%M:%S') ]=====" >> $HOME/log/changes.log
-	echo "===============New file added===============" >> $HOME/log/changes.log
-	echo >> $HOME/log/changes.log
+	echo "===== [ Timestamp: $(TZ=Europe/Paris date '+%Y-%m-%d %H:%M:%S') ]=====" >> $changes_log_p
+	echo "===============New file added===============" >> $changes_log_p
+	echo >> $changes_log_p
 
 	# Put new files inside backup folder
 	while IFS= read -r newfile; do
 		cp "$newfile" "$2/${last_part_dir_check1}"$(awk -F"$1" '{print $2}' <<< "$newfile")
-		echo "Added $2/${last_part_dir_check1}"$(awk -F"$1" '{print $2}' <<< "$newfile") >> $HOME/log/changes.log
-	done < $HOME/log/.new_files.txt
+		echo "Added $2/${last_part_dir_check1}"$(awk -F"$1" '{print $2}' <<< "$newfile") >> $changes_log_p
+	done < $h_new_files_p
 
-	echo >> $HOME/log/changes.log
-	echo "---------------------------------------------" >> $HOME/log/changes.log
-	echo >> $HOME/log/changes.log
+	echo >> $changes_log_p
+	echo "---------------------------------------------" >> $changes_log_p
+	echo >> $changes_log_p
 
 	# Empty the .new_files.txt file => will be done later see line 142
 
@@ -99,6 +107,7 @@ fi
 # Update snapshot
 mv "$current" "$SNAPSHOT"
 
+
 ###########################################################
 
 #-------------------
@@ -106,7 +115,8 @@ mv "$current" "$SNAPSHOT"
 #-------------------
 
 # Remove FAILED file to avoid confusion (test phase)
-rm $HOME/log/FAILED_${gen_date}.log 2>/dev/null
+rm $FAILED_file_p 2>/dev/null
+
 
 ###########################################################
 
@@ -114,19 +124,19 @@ rm $HOME/log/FAILED_${gen_date}.log 2>/dev/null
 # Use checksum to check for changes
 #-----------------------------------
 
-sha256sum -c $HOME/log/dir_checker_${gen_date}.log 2>/dev/null | grep "FAILED" > $HOME/log/FAILED_${gen_date}.log
-find $1 -type f -print0 | xargs -0 -n1 'sha256sum' > /home/kali/log/dir_checker_${gen_date}.log
+sha256sum -c $HOME/log/dir_checker_${gen_date}.log 2>/dev/null | grep "FAILED" > $FAILED_file_p
+find $1 -type f -print0 | xargs -0 -n1 'sha256sum' > $HOME/log/dir_checker_${gen_date}.log
 
-if [[ -s /home/kali/log/FAILED_${gen_date}.log ]]; then
+if [[ -s $FAILED_file_p ]]; then
 	echo "Changes were made, FAILED is not empty."
 
 	# Load list of new files to skip them from diffs
 	mapfile -t new_files_array < "${HOME}/log/.new_files.txt"
 
 	# Compare old file to new file and diff them into changes.log
-	echo "===== [ Timestamp: $(TZ=Europe/Paris date '+%Y-%m-%d %H:%M:%S') ]=====" >> $HOME/log/changes.log
-	echo "================Changes made=================" >> $HOME/log/changes.log
-	echo >> $HOME/log/changes.log
+	echo "===== [ Timestamp: $(TZ=Europe/Paris date '+%Y-%m-%d %H:%M:%S') ]=====" >> $changes_log_p
+	echo "================Changes made=================" >> $changes_log_p
+	echo >> $changes_log_p
 
 	while IFS= read -r rel_path; do
 		skip=false
@@ -148,27 +158,28 @@ if [[ -s /home/kali/log/FAILED_${gen_date}.log ]]; then
 			continue
 		fi
 
-		echo "$2/${last_part_dir_check1}${rel_path}" " >> " "${1}${rel_path}" >> $HOME/log/changes.log
-		diff --color=always "$2/${last_part_dir_check1}${rel_path}" "${1}${rel_path}" >> $HOME/log/changes.log
-		echo >> $HOME/log/changes.log
+		echo "$2/${last_part_dir_check1}${rel_path}" " >> " "${1}${rel_path}" >> $changes_log_p
+		diff --color=always "$2/${last_part_dir_check1}${rel_path}" "${1}${rel_path}" >> $changes_log_p
+		echo >> $changes_log_p
 		cp "${1}${rel_path}" "$2/${last_part_dir_check1}${rel_path}"
-	done < <(awk -F"${1}" '{print $2}' /home/kali/log/FAILED_${gen_date}.log | cut -d ":" -f 1)
+	done < <(awk -F"${1}" '{print $2}' $FAILED_file_p | cut -d ":" -f 1)
 
-	echo >> $HOME/log/changes.log
-	echo "---------------------------------------------" >> $HOME/log/changes.log
-	echo >> $HOME/log/changes.log
+	echo >> $changes_log_p
+	echo "---------------------------------------------" >> $changes_log_p
+	echo >> $changes_log_p
 
 else
 	echo "No changes were made, FAILED is empty."
-	echo "===== [ Timestamp: $(TZ=Europe/Paris date '+%Y-%m-%d %H:%M:%S') ]=====" >> $HOME/log/changes.log
-	echo "==========No changes nor new files==========" >> $HOME/log/changes.log
-	echo >> $HOME/log/changes.log
-	echo "---------------------------------------------" >> $HOME/log/changes.log
-	echo >> $HOME/log/changes.log
+	echo "===== [ Timestamp: $(TZ=Europe/Paris date '+%Y-%m-%d %H:%M:%S') ]=====" >> $changes_log_p
+	echo "==========No changes nor new files==========" >> $changes_log_p
+	echo >> $changes_log_p
+	echo "---------------------------------------------" >> $changes_log_p
+	echo >> $changes_log_p
 
 fi
 
-echo > $HOME/log/.new_files.txt
+echo > $h_new_files_p
+
 
 ###########################################################
 
